4e0b65ec0c49ee6b15a9e021a333b2d84f033537
Menglei Lei <leolei27753@gmail.com>
2017-02-11 19:03:56 -0600

    Add files via upload

---
+++ src/js/mainpage.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $("#strength").click(function(){
+      window.location.href="workout.html";
+  });
+});
---
+++ src/js/workout.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+    $(".test").mouseover(function()
+    {
+        $(this).css("cursor", "pointer");
+        $(this).animate({'opacity': 1, 'margin-top': '0px', 'box-shadow': '50px 50px 50px #999'}, {duration: 500});
+    });
+    $(".test").mouseout(function()
+    {
+        $(this).animate({'opacity': 0.5, 'margin-top': '0px', 'box-shadow': '0px 0px 0px #000'}, {duration: 500});
+    });
+    $("#strength").click(function(){
+        window.location.href="strength.html";
+    });
+    $("#llmj").click(function(){
+      window.location.href = "mainpage.html";
+    });
+    $("#xialastr").click(function(){
+      window.location.href = "strength.html";
+    });
+});
